458ac08ed0378b6dabd9453105285a116376c374
Alexey Black <anra582@gmail.com>
2019-09-16 00:09:38 +0700

    Initial commit

---
+++ src/test/java/com/Anra582/QuestionsServer/QuestionsServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Anra582.QuestionsServer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class QuestionsServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
